9749ba8823cf0e961dc5c7f9648966aa0efae192
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-06-30 00:06:47 +0800

    Setup dashboard for the Main Application

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('template-authentication.login.index');
 });
+
+Route::get('/dashboard', function () {
+  return view('master.content.dashboard.index');
+});
