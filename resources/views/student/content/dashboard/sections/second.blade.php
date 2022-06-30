3096021c7383a9055a59c84bc3b4c41ff4f803d8
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-06-30 23:18:45 +0800

    Additional Setup Student

--- resources/views/student/content/dashboard/sections/second.blade.php
+++ resources/views/student/content/dashboard/sections/second.blade.php
@@ -1,3 +1,9 @@
+<nav id="orders-table-tab" class="orders-table-tab app-nav-tabs nav shadow-sm flex-column flex-sm-row mb-4" role="tablist">
+  <a class="flex-sm-fill text-sm-center nav-link active" id="orders-all-tab" data-bs-toggle="tab" href="#orders-all" role="tab" aria-controls="orders-all" aria-selected="true">All</a>
+  <a class="flex-sm-fill text-sm-center nav-link" id="orders-paid-tab" data-bs-toggle="tab" href="#orders-paid" role="tab" aria-controls="orders-paid" aria-selected="false" tabindex="-1">Pending</a>
+  <a class="flex-sm-fill text-sm-center nav-link" id="orders-pending-tab" data-bs-toggle="tab" href="#orders-pending" role="tab" aria-controls="orders-pending" aria-selected="false" tabindex="-1">Completed</a>
+</nav>
+
 <div class="app-card app-card-chart h-100 shadow-sm">
   <div class="app-card-header p-3">
     <div class="row justify-content-between align-items-center">
@@ -31,6 +37,13 @@
           <td class="cell"><span class="badge bg-warning">Pending</span></td>
           <td class="cell">05/20/2022 07:00:23 AM</td>
           <td class="cell"><button class="btn btn-secondary" type="button">Sign</button></td>
+        </tr>
+        <tr>
+          <td class="cell">3</td>
+          <td class="cell">Enrollment Form</td>
+          <td class="cell"><span class="badge bg-success">Signed</span></td>
+          <td class="cell">05/20/2022 07:00:23 AM</td>
+          <td class="cell"><span class="badge bg-success">Completed</span></td>
         </tr>
       </tbody>
     </table>
